35e4e658d64c1595a044fa8f9429d79431137957
janesh-kiddev <officialjanesh12@gmail.com>
2025-01-01 11:33:54 +0530

    commit changes

---
+++ SimpleExce.java
@@ -0,0 +1,35 @@
+/**public class SimpleExce {
+
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+
+            System.out.println(a/b);
+            
+        } catch (ArithmeticException e) {
+            System.out.println("Error caught");
+        }
+    }
+    
+}*/
+
+
+public class SimpleExce {
+    public static void main(String[] args) {
+        try {
+            age(10);
+        } catch (Exception e) {
+            System.out.println(e.getMessage()+" "+"Caught");
+        }
+    }
+
+     static void age(int num) throws Exception{
+        if(num<18){
+            throw new Exception("Invalid age please try again");
+        }
+        else{
+            System.out.println("You are eligible for the process");
+        }
+    }
+}
